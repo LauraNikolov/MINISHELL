e931ef1e7f9e8681fafcc5b061219d089da364af
renard <melanie.martin0505@gmail.com>
2024-04-14 11:08:29 +0200

    first commit

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    
+    char *buffer = NULL;
+    int buff_size;
+
+    buff_size = 1024;
+    while (getline(buffer, buff_size, 0) != 0) // tant qu il y a une ligne saisie par l user dans le prompt
+    {
+        
+        //ft_display_prompt();
+        //ft_exec_cmd();
+    }
 }

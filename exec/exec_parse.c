ba853061774862cc99acfe25e73dd5ce9c0955a6
Laura Nicoloff <lnicolof@bess-f2r4s16.clusters.42paris.fr>
2024-05-13 14:51:14 +0200

    path enleve

--- exec/exec_parse.c
+++ exec/exec_parse.c
@@ -6,7 +6,7 @@
 /*   By: lnicolof <lnicolof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 11:56:13 by lnicolof          #+#    #+#             */
-/*   Updated: 2024/05/13 14:45:59 by lnicolof         ###   ########.fr       */
+/*   Updated: 2024/05/13 14:50:54 by lnicolof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,33 @@
 
 void ft_build_ast(save_struct *t_struct, char **envp)
 {
-    //je check si y'a qu'une seule commande
-    int cmd_size;
-
-    //je check si y'a des operateurs
-    ft_get_path(t_struct->cmd);
-    cmd_size = ft_lst_size(t_struct->cmd);
+    
     if(cmd_size == 1)
     {
         ft_exec_single_cmd(t_struct, envp);
+        return ;
     }
+    else
+    {
+        
+    }
+    
     //je construit l'ast
 }
 
 
 void ft_exec(save_struct *t_struct, char **envp)
 {
-    ft_build_ast(t_struct, envp);
-    //exec_ast(t_struct);
+    int cmd_size;
+    cmd_size = ft_lst_size(t_struct->cmd);
+    //ft_get_path(t_struct->cmd);
+    if(cmd_size == 1)
+    {
+        ft_exec_single_cmd(t_struct, envp);
+        return ;
+    }
+    else
+    {
+        ft_build_ast(t_struct, envp);
+    }
 }

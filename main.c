6fb0564f0f7f900ed93073d1af8e46eb87db5524
Laura Nicoloff <lnicolof@bess-f2r4s16.clusters.42paris.fr>
2024-05-13 11:11:28 +0200

    modif main

--- main.c
+++ main.c
@@ -25,36 +25,30 @@ void	ft_handler_signals(int signal)
 	}
 }
 
+
 int	main(int ac, char **av, char **envp)
 {
 	char	*buffer;
-	t_cmd	*lst;
-
-	// t_envp	*envp_lst;
+	
 	(void)av;
 	(void)ac;
 	(void)envp;
 	buffer = NULL;
-	lst = NULL;
-	// envp_lst = NULL;
-	// envp_lst = ft_save_envp(envp, &envp_lst);
 	signal(SIGINT, ft_handler_signals);
 	while (1)
 	{
+		save_struct *t_struct;
+		t_struct = malloc(sizeof(save_struct));
+		ft_memset(t_struct, 0, sizeof(*t_struct));
 		buffer = readline(CYAN "MINISHELL~ " RESET);
 		if (!buffer)
-		{
-			free(buffer);
-			// ft_free_envp_lst(envp_lst);
-			return (0);
-		}
-		ft_tokenize(buffer, &lst);
-		// ft_get_path(&lst);
-		// ft_exec_cmd();
-		ft_print_lst(lst);
-		ft_free_lst(lst);
-		lst = NULL;
+			return(free(buffer), 0);
+		ft_tokenize(buffer, &(t_struct->cmd));
+		//ft_print_lst(t_struct->cmd);
+		//ft_free(t_struct);
+		ft_free(t_struct);
 		free(buffer);
 		buffer = NULL;
 	}
 }
+
